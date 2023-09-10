e905c05e968be8a6356fb798ee3b5e19bbd7c2c6
Asanga Joseph <130898120+King-Asanga@users.noreply.github.com>
2023-09-10 07:28:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// JavaScript code to update real-time elements
+const updateRealTimeElements = () => {
+    const dayOfWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+    const utcTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
+
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const now = new Date(); 
+    dayOfWeekElement.textContent = daysOfWeek[now.getUTCDay()];
+    utcTimeElement.textContent = now.toISOString().substr(11, 8) + " UTC";
+    
+};
+
+// Update real-time elements every millisecond
+setInterval(updateRealTimeElements, 1000);
